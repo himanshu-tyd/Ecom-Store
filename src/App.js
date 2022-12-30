e837ebf5e5522df6b30b8f5f07d7d038a3697b3e
Himanshu Taviyad <himanshutaviyad0072@gmail.com>
2022-12-30 12:15:35 +0530

    Added css file for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './App.css';
+import Homepage from './homepage.component';
+
+class App extends Component{
+  render(){
+    return (
+      <div className='homepage'>
+        <Homepage/>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import './homepage.style.scss';
+
+const HomePage = () => (
+  <>
+    <div className="homapage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATES</h1>
+            <span className="subtitle">SHOPE NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="homapage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNEAKERS</h1>
+            <span className="subtitle">SHOPE NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="homapage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">COTHES</h1>
+            <span className="subtitle">SHOPE NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="homapage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MEN</h1>
+            <span className="subtitle">SHOPE NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="homapage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMEN</h1>
+            <span className="subtitle">SHOPE NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+export default HomePage;
